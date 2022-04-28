c5a1659c4fc155f0626c820093534c96c59a7f88
Abhishek Singh <abhishek404singh@gmail.com>
2022-04-28 06:57:00 +0530

    implemented protected routes system

--- components/auth/signupComponent.jsx
+++ components/auth/signupComponent.jsx
@@ -1,5 +1,6 @@
-import { useState } from "react";
-import { signup } from "../../actions/auth";
+import { useState, useEffect } from "react";
+import Router from "next/router";
+import { isAuth, signup } from "../../actions/auth";
 
 const SignupComponent = () => {
   const [values, setValues] = useState({
@@ -11,6 +12,8 @@ const SignupComponent = () => {
     message: "",
     showForm: true,
   });
+
+  useEffect(() => isAuth() && Router.push(`/`), []);
 
   const { name, email, password, error, loading, message, showForm } = values;
 
