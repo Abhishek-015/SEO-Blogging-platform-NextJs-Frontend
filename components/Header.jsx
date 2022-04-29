9537551efb219c88d4f91a694648b25c9febefc8
Abhishek Singh <abhishek404singh@gmail.com>
2022-04-29 14:57:46 +0530

    navbar items toggling wrt user and admin

--- components/Header.jsx
+++ components/Header.jsx
@@ -29,35 +29,56 @@ const Header = () => {
         <Link href="/">
           <NavLink style={{ fontWeight: "bolder" }}>{APP_NAME}</NavLink>
         </Link>
-        <NavbarToggler onClick={toggle} />
-        <Collapse isOpen={isOpen} navbar>
-          <Nav className="ml-auto" navbar>
-            {!isAuth() && (
-              <>
+        <div style={{ display: "flex", justifyContent: "right" }}>
+          <NavbarToggler onClick={toggle} />
+          <Collapse isOpen={isOpen} navbar>
+            <Nav className="ml-auto" navbar>
+              {!isAuth() && (
+                <>
+                  <NavItem>
+                    <Link href="/signin">
+                      <NavLink style={{ cursor: "pointer" }}>Signin</NavLink>
+                    </Link>
+                  </NavItem>
+                  <NavItem>
+                    <Link href="/signup">
+                      <NavLink style={{ cursor: "pointer" }}>Signup</NavLink>
+                    </Link>
+                  </NavItem>
+                </>
+              )}
+
+              {isAuth() && isAuth().role === 0 && (
                 <NavItem>
-                  <Link href="/signin">
-                    <NavLink style={{ cursor: "pointer" }}>Signin</NavLink>
+                  <Link href="/user">
+                    <NavLink style={{ cursor: "pointer" }}>
+                      {`${isAuth().name}'s Dashboard`}
+                    </NavLink>
                   </Link>
                 </NavItem>
+              )}
+              {isAuth() && isAuth().role === 1 && (
                 <NavItem>
-                  <Link href="/signup">
-                    <NavLink style={{ cursor: "pointer" }}>Signup</NavLink>
+                  <Link href="/admin">
+                    <NavLink style={{ cursor: "pointer" }}>
+                      {`${isAuth().name}'s Dashboard`}
+                    </NavLink>
                   </Link>
                 </NavItem>
-              </>
-            )}
-            {isAuth() && (
-              <NavItem>
-                <NavLink
-                  style={{ cursor: "pointer" }}
-                  onClick={() => signout(() => Router.push(`/signin`))}
-                >
-                  Signout
-                </NavLink>
-              </NavItem>
-            )}
-          </Nav>
-        </Collapse>
+              )}
+              {isAuth() && (
+                <NavItem>
+                  <NavLink
+                    style={{ cursor: "pointer" }}
+                    onClick={() => signout(() => Router.push(`/signin`))}
+                  >
+                    Signout
+                  </NavLink>
+                </NavItem>
+              )}
+            </Nav>
+          </Collapse>
+        </div>
       </Navbar>
     </div>
   );
