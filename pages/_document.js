c4c34fb0f9fc19f16dc6e43bac36c938657a9bb6
Abhishek Singh <abhishek404singh@gmail.com>
2022-05-04 00:03:26 +0530

    implementing nprogress feature

--- pages/_document.js
+++ pages/_document.js
@@ -6,7 +6,16 @@ export default function Document() {
       <Head lang="en">
         <meta charSet="UTF-8" />
         <meta name="viewPort" content="width=device-width, initial-scale=1.0" />
-        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" />
+        <link
+          rel="stylesheet"
+          href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css"
+        />
+        {/* nprogress cdn */}
+        <link
+          rel="stylesheet"
+          href="https://cdnout.com/nprogress/nprogress.min.css"
+        />
+        <link rel="stylesheet" href="/static/css/styles.css" />
       </Head>
       <body>
         <Main />
