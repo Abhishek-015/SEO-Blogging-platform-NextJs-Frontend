71328635fe047271cabd949b7288fc7409aa73c6
Abhishek Singh <abhishek404singh@gmail.com>
2022-05-04 10:00:16 +0530

    implemented category creation at adi=min page

---
+++ components/crud/Category.jsx
@@ -0,0 +1,58 @@
+import { useState, useEffect } from "react";
+import Link from "next/link";
+import Router from "next/router";
+import { isAuth, getCookie } from "../../actions/auth";
+import { create } from "../../actions/category";
+
+const Category = () => {
+  const [values, setValues] = useState({
+    name: "",
+    error: false,
+    success: false,
+    categories: [],
+    removed: false,
+  });
+
+  const { name, error, success, categories, removed } = values;
+  const token = getCookie("token");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    create({ name }, token).then((data) => {
+      if (data.error) {
+        setValues({ ...values, error: data.error, success: false });
+      } else {
+        setValues({ ...values, error: false, success: true, name: "" });
+      }
+    });
+  };
+  const handleChange = (e) => {
+    setValues({
+      ...values,
+      name: e.target.value,
+      error: false,
+      success: false,
+      removed: "",
+    });
+  };
+
+  const newCategoryForm = () => (
+    <form onSubmit={handleSubmit}>
+      <div className="form-group">
+        <label className="text-muted">Name</label>
+        <input
+          type="text"
+          className="form-control"
+          onChange={handleChange}
+          value={name}
+        />
+        <button type="submit" className="btn btn-primary btn-sm my-2">
+          Create
+        </button>
+      </div>
+    </form>
+  );
+  return <>{newCategoryForm()}</>;
+};
+
+export default Category;
--- pages/admin/crud/category-tag.js
+++ pages/admin/crud/category-tag.js
@@ -1,6 +1,7 @@
 import Layout from "../../../components/Layout";
 import Admin from "../../../components/auth/Admin";
 import Link from "next/link";
+import Category from "../../../components/crud/Category";
 
 const CategoryTag = () => {
   return (
@@ -12,7 +13,7 @@ const CategoryTag = () => {
               <h3>Manage Categories and Tags</h3>
             </div>
             <div className="col-md-6">
-              <p>Categories</p>
+              <Category />
             </div>
             <div className="col-md-6">
               <p>tags</p>
