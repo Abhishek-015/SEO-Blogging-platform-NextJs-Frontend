1fd154ac03545c0a41cc2f98b515b122ab565373
Abhishek Singh <abhishek404singh@gmail.com>
2022-05-04 07:48:00 +0530

    created category-tag page to render category and tags in admin dashboard

---
+++ pages/admin/crud/category-tag.js
@@ -0,0 +1,26 @@
+import Layout from "../../../components/Layout";
+import Admin from "../../../components/auth/Admin";
+import Link from "next/link";
+
+const CategoryTag = () => {
+  return (
+    <Layout>
+      <Admin>
+        <div className="container-fluid">
+          <div className="row">
+            <div className="col-md-12 py-3 ">
+              <h3>Manage Categories and Tags</h3>
+            </div>
+            <div className="col-md-6">
+              <p>Categories</p>
+            </div>
+            <div className="col-md-6">
+              <p>tags</p>
+            </div>
+          </div>
+        </div>
+      </Admin>
+    </Layout>
+  );
+};
+export default CategoryTag;
--- pages/admin/index.js
+++ pages/admin/index.js
@@ -6,7 +6,23 @@ const AdminIndex = () => {
   return (
     <Layout>
       <Admin>
-        <h2>Admin Dashboard</h2>
+        <div className="container-fluid">
+          <div className="row">
+            <div className="col-md-12 py-3 ">
+              <h2>Admin Dashboard</h2>
+            </div>
+            <div className="col-md-4">
+              <ul className="list-group">
+                <li className="list-group-item">
+                  <Link href='/admin/crud/category-tag'>
+                    <a>Create category</a>
+                  </Link>
+                </li>
+              </ul>
+            </div>
+            <div className="col-md-8">right</div>
+          </div>
+        </div>
       </Admin>
     </Layout>
   );
